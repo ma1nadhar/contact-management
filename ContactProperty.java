162b76a93d503ee6a6c8835149db3398e458256d
Maina Dhar <dmaina2244@gmail.com>
2022-09-04 11:58:01 -0700

    added comments

--- ContactProperty.java
+++ ContactProperty.java
@@ -1,3 +1,14 @@
+/* 
+ * ContactProperty.java
+ * 
+ * Maina Dhar
+ * Java Project - Property Class
+ * 
+ * This java project incorporates and deletes contact details. The system 
+ * will let customers add contacts, with names and email addresses. Below are
+ * the attributes used in ConsoleContactManagement.java.
+ * 
+ */
 
 public class ContactProperty {
 	
